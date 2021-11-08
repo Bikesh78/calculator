01647458d8c3ca19b00038cf7fe2c9be2946afd5
Bikesh Rai <bikeshrai78@gmail.com>
2021-11-08 18:02:15 +0545

    Show clicked number on screen

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,22 @@ function operate (num1, operator, num2){
     if(operator = '*') return multiply(num1,num2);
     if(operator = '/') return divide(num1,num2); 
 }
+let temp = '';
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => {
+    btn.addEventListener('click',(e) => {
+        
+        if (e.target.classList.contains("number")){
+            temp +=e.target.textContent; 
+        } else if (e.target.classList.contains("del")){  
+                let newTemp = temp.slice(0,temp.length-1);
+                temp= newTemp;
+        } 
+        const displayNumber = document.querySelector('.displayNum');
+        displayNumber.textContent = temp;
+        if (e.target.classList.contains("clear")){
+            temp = '';
+            displayNumber.textContent = '0';
+        }           
+    });
+});
